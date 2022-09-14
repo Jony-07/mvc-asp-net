745af9cc4b0f2b7fa44f99524c21159a5fec72bb
Jony-07 <jony25lopezml@gmail.com>
2022-09-13 21:48:49 -0600

    Avance

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -14,8 +14,8 @@ namespace ticketEccommerce.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allMovies = await _context.Movies.ToListAsync();
-            return View();
+            var allMovies = await _context.Movies.Include(n => n.Cinema).ToListAsync();
+            return View(allMovies);
         }
     }
 }
