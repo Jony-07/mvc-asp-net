fbe645d545d056844cd48f665ea62b5dcf64a7a6
Jony-07 <jony25lopezml@gmail.com>
2022-10-27 23:17:09 -0600

    Fixed bud 👾

--- Models/Order.cs
+++ Models/Order.cs
@@ -11,7 +11,7 @@ namespace ticketEccommerce.Models
         public string Email { get; set; }
 
         public string UserId { get; set; }
-        [ForeignKey(nameof(UserId))]
+        //[ForeignKey(nameof(UserId))]
         //public ApplicationUser User { get; set; }
 
         public List<OrderItem> OrderItems { get; set; }
